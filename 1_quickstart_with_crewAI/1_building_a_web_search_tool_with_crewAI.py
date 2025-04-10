f7ad17a8193a1eab816ed070babe70f80c1ddd06
EddyGiusepe <eddychirinos.unac@gmail.com>
2025-04-10 19:00:41 -0300

    Primeiro agente com crewAI

---
+++ 1_quickstart_with_crewAI/1_building_a_web_search_tool_with_crewAI.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Datacamp ---> https://www.datacamp.com/tutorial/crew-ai
+
+crewAi --> https://docs.crewai.com/introduction
+"""
+
+
