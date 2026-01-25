615436898c8217cf3fdc5468f920b6f6eb090b00
EddyGiusepe <eddychirinos.unac@gmail.com>
2026-01-25 00:44:03 -0300

    style: ansi colors para o terminal

---
+++ 3_rag_agent_with_crewai/ansi_colors.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Script ansi_colors.py
+=====================
+Este script contém códigos ANSI para formatação de cores no terminal.
+"""
+from logging_config import (
+    get_logger,
+    setup_logging,
+)
+
+setup_logging()
+logger = get_logger(__name__)
+
+# ANSI codes for colors:
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+MAGENTA = "\033[95m"
+CYAN = "\033[96m"
+RESET = "\033[0m"
+
+
+if __name__ == "__main__":
+    logger.info(f"{RED}Hello, World!{RESET}")
